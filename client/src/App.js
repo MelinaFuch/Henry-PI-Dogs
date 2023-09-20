c633b4f9586b0d5500a731e9b00f5d6a1999bdab
Melina Fuch <sosafuchmelina@gmail.com>
2023-09-20 10:40:32 -0300

    back

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Detail from './components/Detail/Detail';
 // import SearchBar from './components/SearchBar/SearchBar';
 import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://henry-pi-dogs-production.up.railway.app/';
+axios.defaults.baseURL = 'https://henry-pi-dogs-production.up.railway.app';
 
 function App() {
   return (
